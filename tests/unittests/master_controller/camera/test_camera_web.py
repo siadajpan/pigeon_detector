60da288c0217d6c1d45279e59d9d62581498a304
Karol <siadajpan@o2.pl>
2020-01-03 14:00:25 +0100

    Fix tests for web camera

--- tests/unittests/master_controller/camera/test_camera_web.py
+++ tests/unittests/master_controller/camera/test_camera_web.py
@@ -28,24 +28,56 @@ class TestCameraWeb(TestCase):
 
     def _stop_loop(self):
         self.camera_web._stop_camera = True
+        return MagicMock(), MagicMock()
 
     def test_run_with_showing_image(self):
         # given
         self.camera_web.showing_image = True
         self.camera_web.show_frame = MagicMock()
-        # self.cap.read.side_effect = self._stop_loop
+        self.cap.read.side_effect = self._stop_loop
         
         # when
         self.camera_web.run()
         
         # then
         self.cap.read.assert_called()
+        self.camera_web.show_frame.assert_called()
 
-    def test_quit(self):
+    def test_run_without_showing_image(self):
         # given
-        
+        self.camera_web.showing_image = False
+        self.camera_web.show_frame = MagicMock()
+        self.cap.read.side_effect = self._stop_loop
+
         # when
-        
+        self.camera_web.run()
+
         # then
-        pass
+        self.cap.read.assert_called()
+        self.camera_web.show_frame.assert_not_called()
+
+    def test_quit_showing_image(self):
+        # given
+        self.camera_web.showing_image = True
+        self.camera_web.end_showing = MagicMock()
+
+        # when
+        self.camera_web.quit()
+
+        # then
+        self.camera_web.end_showing.assert_called()
+        self.cap.release.assert_called()
+
+    def test_quit_not_showing_image(self):
+        # given
+        self.camera_web.showing_image = False
+        self.camera_web.end_showing = MagicMock()
+
+        # when
+        self.camera_web.quit()
+
+        # then
+        self.camera_web.end_showing.assert_not_called()
+        self.cap.release.assert_called()
+
 
