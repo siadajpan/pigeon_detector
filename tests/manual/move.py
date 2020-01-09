eec762af2d83a26b61aebce93ac6fc373e35d5a1
siadajpan <siadajpan@o2.pl>
2020-01-09 11:30:22 +0100

    Revert "Feature/manual tests"

--- tests/manual/move.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-import settings
-
-if __name__ == '__main__':
-    movement_path = settings.Server.MOVEMENT_ADDRESSES[0] + \
-                    settings.Server.MOVEMENT
-    requests.post(movement_path)
