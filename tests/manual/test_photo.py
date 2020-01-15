b94812549bc5b2d091f4fed8ab447cbdab9b4a6c
siadajpan <siadajpan@o2.pl>
2020-01-15 10:54:22 +0100

    Add frame resolution to make photo test

--- tests/manual/test_photo.py
+++ tests/manual/test_photo.py
@@ -6,6 +6,8 @@ import cv2
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     time.sleep(1)
     ret, frame = cap.read()
     now = datetime.datetime.now()
