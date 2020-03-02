fecfe7a6237636e40fec93f37e279e74d39b0a31
Karol <siadajpan@o2.pl>
2020-03-02 16:39:19 +0100

    Update contours calculation and detection area as list

--- master_controller/image_preprocessing/movement_detector.py
+++ master_controller/image_preprocessing/movement_detector.py
@@ -1,8 +1,10 @@
-from threading import Thread
-from typing import Optional
 import time
+from threading import Thread
+from typing import Optional, List
+
 import cv2
 import numpy as np
+
 import settings
 from master_controller.image_preprocessing.rectangle import Rectangle
 
@@ -10,31 +12,28 @@ from master_controller.image_preprocessing.rectangle import Rectangle
 class MovementDetector(Thread):
     def __init__(self):
         super().__init__()
-        self.birds_detected = False
         self.curr_frame = None
         self.base_frame = None
-        self.movement_frame = None
         self.base_frame_time = 0.
         self.stop_detector = False
-        self.movement_area = Rectangle(0, 0, 0, 0)
-        self.use_self_detector = True
+        self.movement_area: Optional[List[Rectangle]] = None
 
     def stop(self):
         self.stop_detector = True
-        
+
     def add_frame(self, frame):
         self.curr_frame = frame
 
     @staticmethod
-    def preprocess_image(image):
+    def pre_process_image(image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        
+
         return gray
-        
+
     def update_base_frame(self, frame):
         self.base_frame = frame.copy()
-        self.base_frame = self.preprocess_image(self.base_frame)
+        self.base_frame = self.pre_process_image(self.base_frame)
         self.base_frame_time = time.time()
 
     @staticmethod
@@ -55,58 +54,33 @@ class MovementDetector(Thread):
 
         return None
 
-    def find_contour(self, bin_frame: np.array):
+    @staticmethod
+    def find_contour(bin_frame: np.array):
         # finds rectangle contour around all non-zero elements in binary array
-        indexes = []
-        height, width = bin_frame.shape[:2]
+        contours, _ = cv2.findContours(bin_frame, cv2.RETR_TREE,
+                                       cv2.CHAIN_APPROX_SIMPLE)
 
-        for i in range(4):
-            index = self.find_first_non_zero_row(bin_frame)
+        rectangles = [Rectangle(*cv2.boundingRect(contour))
+                      for contour in contours]
 
-            if index is None:
-                return None
-
-            indexes.append(index)
-            bin_frame = np.rot90(bin_frame)
-
-        y_top, x_right, y_bot, x_left = indexes
-        x_width = width - x_right - x_left
-        y_height = height - y_bot - y_top
-
-        return Rectangle(x_left, y_top, x_width, y_height)
+        return rectangles
 
     @staticmethod
     def find_array_difference(base_frame: np.array, gray_array: np.array):
         frame_diff = cv2.absdiff(base_frame, gray_array)
-        thresh = cv2.threshold(frame_diff, 55, 255, cv2.THRESH_BINARY)[1]
-        return cv2.dilate(thresh, None, iterations=2)
+        min_pixel_difference = settings.PreProcessing.MIN_PIXEL_DIFFERENCE
+        _, thresh = cv2.threshold(frame_diff, min_pixel_difference, 255,
+                                  cv2.THRESH_BINARY)
+        kernel = np.ones((51, 51))
+        dilation = cv2.dilate(thresh, kernel)
+        erosion = cv2.erode(dilation, kernel)
 
-    def select_movement_contours(self, frame):
-        gray = self.preprocess_image(frame)
+        return erosion
+
+    def select_movement_contours(self):
+        gray = self.pre_process_image(self.curr_frame)
         difference_binary = self.find_array_difference(self.base_frame, gray)
         self.movement_area = self.find_contour(difference_binary)
-
-    def cut_frame_by_movement(self, frame):
-        self.select_movement_contours(frame)
-        if self.movement_area:
-            frame = self.cut_frame(frame, self.movement_area)
-            return frame
-
-        return None
-
-    def preprocess_frame(self):
-        frame = self.curr_frame.copy()
-        frame = self.cut_frame_by_movement(frame)
-
-        if frame is None:
-            self.movement_frame = None
-            return
-
-        if frame.shape[0] < 4 or frame.shape[1] < 4:
-            self.movement_frame = None
-            return
-
-        self.movement_frame = frame
 
     def check_if_update_base_frame(self):
         return time.time() - self.base_frame_time > settings.NEW_BASE_TIME
@@ -120,4 +94,4 @@ class MovementDetector(Thread):
             if self.check_if_update_base_frame():
                 self.update_base_frame(self.curr_frame)
 
-            self.preprocess_frame()
+            self.select_movement_contours()
--- tests/unittests/master_controller/image_preprocessing/test_movement_detector.py
+++ tests/unittests/master_controller/image_preprocessing/test_movement_detector.py
@@ -1,6 +1,6 @@
 import time
 from unittest import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import cv2
 import numpy as np
@@ -46,7 +46,7 @@ class TestMovementDetector(TestCase):
         blurred = cv2.GaussianBlur(gray, (21, 21), 0)
 
         # when
-        preprocessed = self.detector.preprocess_image(frame)
+        preprocessed = self.detector.pre_process_image(frame)
 
         # then
         self.assertTrue(np.array_equal(blurred, preprocessed))
@@ -56,7 +56,7 @@ class TestMovementDetector(TestCase):
         frame1 = (np.random.rand(100, 120, 3) * 255).astype(np.uint8)
         frame2 = (np.random.rand(100, 120, 3) * 255).astype(np.uint8)
         time_now = time.time()
-        self.detector.preprocess_image = MagicMock(return_value=frame2)
+        self.detector.pre_process_image = MagicMock(return_value=frame2)
 
         # when
         self.detector.update_base_frame(frame1)
@@ -98,158 +98,57 @@ class TestMovementDetector(TestCase):
         # then
         self.assertEqual(first_row, 2)
 
-    def test_find_contour_empty(self):
+    @patch('cv2.findContours')
+    @patch('cv2.boundingRect')
+    def test_find_contour(self, box_mock, find_contours_mock):
         # given
-        test_array = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-        test_array = np.array(test_array)
+        find_contours_mock.return_value = (['cnts'], 'hierarchy')
+        box_mock.return_value = [1, 2, 3, 4]
 
         # when
-        indexes = self.detector.find_contour(test_array)
+        rectangles = self.detector.find_contour(MagicMock())
 
         # then
-        self.assertIsNone(indexes)
+        find_contours_mock.assert_called()
+        box_mock.assert_called()
+        self.assertEqual([Rectangle(1, 2, 3, 4)], rectangles)
 
-    def test_find_contour(self):
+    @patch('cv2.absdiff')
+    @patch('cv2.threshold')
+    @patch('cv2.dilate')
+    @patch('cv2.erode')
+    def test_find_array_difference(self, erode_mock, dilate_mock, thresh_mock,
+                                   absdiff_mock):
         # given
-        test_array = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 1]]
-        test_array = np.array(test_array)
+        dilate_mock.return_value = 'dilated'
+        erode_mock.return_value = 'eroded'
+        thresh_mock.return_value = ('_', 'thresh')
+        absdiff_mock.return_value = 'absdiff'
 
         # when
-        x_0, y_0, x_width, y_height = \
-            self.detector.find_contour(test_array).data
+        diff_calculated = self.detector.find_array_difference(MagicMock(),
+                                                              MagicMock())
 
         # then
-        self.assertEqual(x_0, 2)
-        self.assertEqual(x_width, 2)
-        self.assertEqual(y_0, 2)
-        self.assertEqual(y_height, 1)
-
-    def test_find_contour_bigger_array(self):
-        # given
-        test_array = [[0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0],
-                      [0, 1, 0, 0, 0],
-                      [0, 0, 1, 1, 0],
-                      [0, 0, 1, 1, 0],
-                      [0, 0, 1, 1, 0]]
-        test_array = np.array(test_array)
-
-        # when
-        x_0, y_0, x_width, y_height = \
-            self.detector.find_contour(test_array).data
-
-        # then
-        self.assertEqual(x_0, 1)
-        self.assertEqual(x_width, 3)
-        self.assertEqual(y_0, 2)
-        self.assertEqual(y_height, 4)
-
-    def test_find_array_difference(self):
-        # given
-        base_frame = (np.random.rand(10, 12, 3) * 255).astype(np.uint8)
-        frame = (np.random.rand(10, 12, 3) * 255).astype(np.uint8)
-        frame_diff = cv2.absdiff(base_frame, frame)
-        thresh = cv2.threshold(frame_diff, 55, 255, cv2.THRESH_BINARY)[1]
-        diff = cv2.dilate(thresh, None, iterations=2)
-
-        # when
-        diff_calculated = self.detector.find_array_difference(base_frame, frame)
-
-        # then
-        self.assertTrue(np.array_equal(diff, diff_calculated))
+        absdiff_mock.assert_called()
+        thresh_mock.assert_called()
+        dilate_mock.assert_called()
+        erode_mock.assert_called()
+        self.assertEqual('eroded', diff_calculated)
 
     def test_select_movement_contours(self):
         # given
-        frame1 = (np.random.rand(10, 12, 3) * 255).astype(np.uint8)
-        frame2 = (np.random.rand(10, 12, 3) * 255).astype(np.uint8)
-        frame3 = (np.random.rand(10, 12, 3) * 255).astype(np.uint8)
-
-        self.detector.preprocess_image = MagicMock(return_value=frame2)
-        self.detector.find_array_difference = MagicMock(return_value=frame3)
-        self.detector.find_contour = MagicMock(
-            return_value=Rectangle(1, 2, 3, 4)
-        )
+        self.detector.pre_process_image = MagicMock()
+        self.detector.find_array_difference = MagicMock()
+        self.detector.find_contour = MagicMock()
 
         # when
-        self.detector.select_movement_contours(frame1)
+        self.detector.select_movement_contours()
 
         # then
-        self.detector.preprocess_image.assert_called_with(frame1)
-        self.detector.find_array_difference.assert_called_with(
-            self.detector.base_frame, frame2
-        )
-        self.detector.find_contour.assert_called_with(frame3)
-        self.assertTrue(np.array_equal(self.detector.movement_area.data,
-                                       (1, 2, 3, 4)))
-
-    def test_cut_frame_by_movement_no_area(self):
-        # given
-        frame = (np.random.rand(10, 12, 3) * 255).astype(np.uint8)
-        self.detector.select_movement_contours = MagicMock()
-        self.detector.movement_area = None
-
-        # when
-        out_frame = self.detector.cut_frame_by_movement(frame)
-
-        # then
-        self.assertIsNone(out_frame)
-
-    def test_cut_frame_by_movement(self):
-        # given
-        frame = (np.random.rand(10, 12, 3) * 255).astype(np.uint8)
-        self.detector.select_movement_contours = MagicMock()
-        self.detector.movement_area = Rectangle(1, 2, 3, 4)
-
-        # when
-        out_frame = self.detector.cut_frame_by_movement(frame)
-
-        # then
-        self.assertTrue(np.array_equal(frame[2: 6, 1: 4], out_frame))
-
-    def test_preprocess_frame_no_movement(self):
-        # given
-        curr_frame = (np.random.rand(40, 60, 3) * 255).astype(np.uint8)
-        cut_frame = None
-        self.detector.curr_frame = curr_frame
-        self.detector.cut_frame_by_movement = MagicMock(
-            return_value=cut_frame
-        )
-
-        # when
-        self.detector.preprocess_frame()
-
-        # then
-        self.assertIsNone(self.detector.movement_frame)
-
-    def test_preprocess_frame_small_movement(self):
-        # given
-        curr_frame = (np.random.rand(40, 60, 3) * 255).astype(np.uint8)
-        cut_frame = (np.random.rand(1, 2, 3) * 255).astype(np.uint8)
-        self.detector.curr_frame = curr_frame
-        self.detector.cut_frame_by_movement = MagicMock(
-            return_value=cut_frame
-        )
-
-        # when
-        self.detector.preprocess_frame()
-
-        # then
-        self.assertIsNone(self.detector.movement_frame)
-
-    def test_preprocess_frame(self):
-        # given
-        curr_frame = (np.random.rand(40, 60, 3) * 255).astype(np.uint8)
-        cut_frame = (np.random.rand(30, 30, 3) * 255).astype(np.uint8)
-        self.detector.curr_frame = curr_frame
-        self.detector.cut_frame_by_movement = MagicMock(
-            return_value=cut_frame
-        )
-
-        # when
-        self.detector.preprocess_frame()
-
-        # then
-        self.assertTrue(np.array_equal(cut_frame, self.detector.movement_frame))
+        self.detector.pre_process_image.assert_called()
+        self.detector.find_array_difference.assert_called()
+        self.detector.find_contour.assert_called()
 
     def test_check_if_update_base_frame_yes(self):
         # given
